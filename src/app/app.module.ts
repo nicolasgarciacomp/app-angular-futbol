a9189e95a70564336c1c523287db00c37679517d
nicolasgarciacomp <nicolasgarciacomp@gmail.com>
2020-01-23 17:38:11 +0000

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClubesComponent } from './components/clubes/clubes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ClubesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClubesComponent } from './components/clubes/clubes.component';
+
+const APP_ROUTES: Routes = [
+	{path: 'home', component: HomeComponent},
+	{path: 'about', component: AboutComponent},
+	{path: 'clubes', component: ClubesComponent},
+	{path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
